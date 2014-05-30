bf7e4b86b2b6148fd8921390f71a0d79003dc253
gejben <gejben@gmail.com>
2014-05-31 00:46:41 +0200

    A* algorithm

--- PathFinding/Game.cpp
+++ PathFinding/Game.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <vector>
 #include <memory>
+#include <algorithm>
+#include <math.h>
 
 bool gameover = false;
 
@@ -18,17 +20,25 @@ struct Goal{
 }goal;
 
 struct Player{
-	int x;
-	int y;
+	double x;
+	double y;
 } playerStart;
 
 struct PlayerState{
 	shared_ptr<Player> player;
-	shared_ptr<Player> parentPlayer;
+	shared_ptr<PlayerState> parentState;
+	double g = 0;
+	double h = 0;
+	double f = 0;
 };
 
-shared_ptr<PlayerState> winningState;
 
+bool player_sorter(shared_ptr<PlayerState> const lhs, shared_ptr<PlayerState> const rhs){
+	return lhs->f < rhs->f;
+}
+
+shared_ptr<PlayerState> winningState;
+shared_ptr<PlayerState> first;
 std::vector<shared_ptr<PlayerState>> open;
 std::vector<shared_ptr<PlayerState>> visited;
 
@@ -99,14 +109,19 @@ bool game_init(HWND hWnd) {
 	playerStart.y = y;
 
 
-	shared_ptr<PlayerState> first = make_shared<PlayerState>();
+	first = make_shared<PlayerState>();
 	first->player = make_shared<Player>(playerStart);
 	open.push_back(first);
 
 	return true;
 }
 
-static void testDirection(int x, int y, shared_ptr<Player> player){
+static double lengthToGoal(int x, int y){
+	double a = (sqrt(abs(x - goal.x) ^ 2) + sqrt(abs(y - goal.y) ^ 2));
+	return a;
+}
+
+static void testDirection(double x, double y, shared_ptr<PlayerState> state){
 	bool ok = true;
 	for each(shared_ptr<PlayerState> state in visited){
 		if((state->player->x == x) && (state->player->y == y)){
@@ -119,45 +134,50 @@ static void testDirection(int x, int y, shared_ptr<Player> player){
 		newState->player = make_shared < Player >() ;
 		newState->player->x = x;
 		newState->player->y = y;
-		newState->parentPlayer = player;
+		newState->parentState = state;
+		newState->g = 0;
+		newState->h = lengthToGoal(x, y);
+		newState->f = newState->g + newState->h;
 		open.push_back(newState);
 		visited.push_back(newState);
 	}
 }
 
-static void checkSurrounding(shared_ptr<Player> player){
+static void checkSurrounding(shared_ptr<PlayerState> state){
 	int x, y;
-	x = player->x - 1;
-	y = player->y;
+	x = state->player->x - 1;
+	y = state->player->y;
 	if(map[x][y] == GRASS){
-		testDirection(x, y, player);
+		testDirection(x, y, state);
 	}
 
 
-	x = player->x;
-	y = player->y - 1;
+	x = state->player->x;
+	y = state->player->y - 1;
 	if(map[x][y] == GRASS){
-		testDirection(x, y, player);
+		testDirection(x, y, state);
 	}
 
-	x = player->x + 1;
-	y = player->y;
+	x = state->player->x + 1;
+	y = state->player->y;
 	if(map[x][y] == GRASS){
-		testDirection(x, y, player);
+		testDirection(x, y, state);
 	}
 
-	x = player->x;
-	y = player->y + 1;
+	x = state->player->x;
+	y = state->player->y + 1;
 	if(map[x][y] == GRASS){
-		testDirection(x, y, player);
+		testDirection(x, y, state);
 	}
+
+	std::sort(open.begin(), open.end(),&player_sorter);
 }
 
 void game_update() {
 	if(searching && open.size() > 0){
 			shared_ptr<PlayerState> pState = open.at(0);
 			open.erase(open.begin());
-			checkSurrounding(pState->player);
+			checkSurrounding(pState);
 			if((pState->player->x == goal.x) && (pState->player->y == goal.y)){
 				winningState = pState;
 				searching = false;
@@ -185,7 +205,15 @@ void game_graphics_update() {
 	}
 
 	DrawTexture(&goalTexture, goal.x*TileSize, goal.y*TileSize);
-	for each (shared_ptr<PlayerState> state in visited){
-		DrawTexture(&playerTexture, state->player->x*TileSize, state->player->y*TileSize);
+	if(searching){
+		for each (shared_ptr<PlayerState> state in visited){
+			DrawTexture(&playerTexture, state->player->x*TileSize, state->player->y*TileSize);
+		}
+	} else{
+		shared_ptr<PlayerState> temp = winningState;
+		while(temp != first){
+			DrawTexture(&playerTexture, temp->player->x*TileSize, temp->player->y*TileSize);
+			temp = temp->parentState;
+		}
 	}
 }
